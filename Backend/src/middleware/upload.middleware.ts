eaf6870abdcbb4ea53ef8269d294a1471f639329
mekdes wale <mekdesw60@gmail.com>
2026-04-10 20:31:45 +0300

    modified

--- Backend/src/middleware/upload.middleware.ts
+++ Backend/src/middleware/upload.middleware.ts
@@ -2,7 +2,6 @@ import multer from 'multer';
 import path from 'path';
 import fs from 'fs';
 
-
 const uploadDir = 'uploads/covers';
 if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir, { recursive: true });
