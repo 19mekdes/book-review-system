cf4686e9f103be5db36063894e5aeb960a1f7d99
mekdes wale <mekdesw60@gmail.com>
2026-07-16 16:34:33 +0300

    update

--- BOOK/src/features/categories/categoriesTypes.ts
+++ BOOK/src/features/categories/categoriesTypes.ts
@@ -1,7 +1,5 @@
-// ============================================
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // Category Types
-// ============================================
-
 export interface Category {
   id: number;
   name: string;
